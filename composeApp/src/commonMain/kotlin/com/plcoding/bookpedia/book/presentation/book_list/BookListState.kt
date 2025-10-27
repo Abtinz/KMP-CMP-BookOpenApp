5020e0ee66bd0e19699114f7e765d72595cdcd0a
Abtinz <Abtinzandi@gmail.com>
2025-10-27 14:36:32 -0400

    UiText and BooklistState

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -3,6 +3,19 @@ package com.plcoding.bookpedia.book.presentation.book_list
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.UiText
 
+/**
+ * Represents the state of the book list screen.
+ *
+ * This data class holds all the necessary information to render the UI for the book list,
+ * including search queries, results, favorite books, loading status, and error messages.
+ *
+ * @property searchQuery The current text entered by the user in the search bar. Defaults to "Kotlin".
+ * @property searchResults The list of books returned from the search query.
+ * @property favoriteBooks The list of books that the user has marked as favorites.
+ * @property isLoading A boolean flag indicating whether a data loading operation (like a search) is in progress.
+ * @property selectedTabIndex The index of the currently selected tab (e.g., 0 for "Search", 1 for "Favorites").
+ * @property errorMessage A user-facing error message, wrapped in a [UiText] for localization. Null if there is no error.
+ */
 data class BookListState(
     val searchQuery: String = "Kotlin",
     val searchResults: List<Book> = emptyList(),
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/UiText.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/UiText.kt
@@ -5,8 +5,33 @@ import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
 
 
+/**
+ * A sealed interface to represent text that can be displayed in the UI.
+ * This is a wrapper that allows for handling both dynamic strings from a data source (e.g., an API)
+ * and static strings from local resources (e.g., strings.xml) in a unified way,
+ * especially within ViewModels.
+ */
 sealed interface UiText {
-    data class DynamicString(val value: String): UiText
+
+    /**
+     * Represents a UI text that is a simple, dynamic string value.
+     * This is used for text that is generated at runtime and doesn't come from a static resource file.
+     *
+     * @property value The actual String content to be displayed.
+     */
+    data class DynamicString(
+        val value: String
+    ): UiText
+
+    /**
+     * Represents a piece of text that comes from a string resource.
+     * This is useful for internationalization (i18n) and for keeping UI strings
+     * in a central, manageable location (resources).
+     *
+     * @property id The [StringResource] identifier for the string.
+     * @property args Optional variable arguments to be formatted into the string resource.
+     *              This corresponds to placeholders like %1$s, %2$d, etc., in the resource string.
+     */
     class StringResourceId(
         val id: StringResource,
         val args: Array<Any> = arrayOf()

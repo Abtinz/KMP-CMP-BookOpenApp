40621666b4d2c7fc49b01a63b9321596f83b4c98
Abtinz <Abtinzandi@gmail.com>
2025-10-28 14:38:14 -0400

    actual implementation of our platform modules, here we will have database factory way different from android

--- composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/di/Modules.ios.kt
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/di/Modules.ios.kt
@@ -6,6 +6,15 @@ import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+/**
+ * A Koin module that provides platform-specific dependencies for the iOS target.
+ *
+ * This module configures the following dependencies:
+ * - [HttpClientEngine]: Provides the `Darwin` engine for Ktor, which is the native
+ *   networking engine for iOS and other Apple platforms.
+ * - [DatabaseFactory]: Provides the factory for creating the platform-specific
+ *   database driver instance (SQLite).
+ */
 actual val platformModule: Module
     get() = module {
         single<HttpClientEngine> { Darwin.create() }

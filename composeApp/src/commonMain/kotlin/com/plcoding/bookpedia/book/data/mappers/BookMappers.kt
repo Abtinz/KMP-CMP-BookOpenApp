0c7232be1536d59171fb0a1a570c942371458b79
Abtinz <Abtinzandi@gmail.com>
2025-10-28 11:22:06 -0400

    mappers for decoupling more domain and data

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
@@ -4,6 +4,17 @@ import com.plcoding.bookpedia.book.data.database.BookEntity
 import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
 import com.plcoding.bookpedia.book.domain.Book
 
+/**
+ * Maps a [SearchedBookDto] from the Open Library API to a [Book] domain model.
+ *
+ * This function handles the transformation of data from the network layer (DTO)
+ * to the domain layer. It extracts the book ID from the full key, constructs the
+ * cover image URL using the available keys, and handles nullable fields by providing
+ * default or empty values.
+ *
+ * @receiver The [SearchedBookDto] instance to map.
+ * @return A [Book] object populated with data from the DTO.
+ */
 fun SearchedBookDto.toBook(): Book {
     return Book(
         id = id.substringAfterLast("/"),
@@ -24,6 +35,12 @@ fun SearchedBookDto.toBook(): Book {
     )
 }
 
+/**
+ * Maps a [Book] domain model to a [BookEntity] for database storage.
+ * This is an extension function on the [Book] class.
+ *
+ * @return A [BookEntity] instance with data mapped from the [Book] object.
+ */
 fun Book.toBookEntity(): BookEntity {
     return BookEntity(
         id = id,
